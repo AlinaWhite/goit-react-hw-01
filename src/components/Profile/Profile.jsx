c41ba37748ea7762fda8b9f11f65d84cda1612d8
Alina White <164808035+AlinaWhite@users.noreply.github.com>
2024-12-13 22:46:23 +0000

    task!

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,30 @@
 import PropTypes from "prop-types";
-import css from "./Profile.css";
+import css from "./Profile.modul.css";
 
 const Profile = (props) => {
   return (
     <div>
       <div>
-        <img src={props.image} alt="User avatar" />
+        <div className={css.profileAvatar}>
+          <img src={props.image} alt="User avatar" />
+        </div>
         <p>{props.name}</p>
-        <p>{props.tag}</p>
+        <p>@{props.tag}</p>
         <p>{props.location}</p>
       </div>
 
       <ul>
         <li>
           <span>Followers</span>
-          <span>{props.followers}</span>
+          <span>{props.stats.followers}</span>
         </li>
         <li>
           <span>Views</span>
-          <span>{props.views}</span>
+          <span>{props.stats.views}</span>
         </li>
         <li>
           <span>Likes</span>
-          <span>{props.likes}</span>
+          <span>{props.stats.likes}</span>
         </li>
       </ul>
     </div>
